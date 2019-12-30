8dd681fb3cc55e6ada0d4e7d410609123ef254d8
ClaesPhilip <claesphiliplofgren@hotmail.com>
2019-12-30 19:25:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// NAVIGATION
+import Nav from "./components/Nav";
+// PAGES
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
 
+const App = () => (
+    <div>
+        <Nav />
+        <div className="container">
+            <Route exact={true} path="/" component={Home} />
+            <Route exaxt path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+        </div>
+    </div>
+);
 
-
+render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
+);
